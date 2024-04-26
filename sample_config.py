4320eca08e46805d6a8a494cd490aeb4db30f144
aditya94139 <152467027+aditya94139@users.noreply.github.com>
2024-04-27 00:12:06 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6608265446:AAFEghuTU_2nt6UascMnPtgTmd6XWqY5PuE")
-    APP_ID = int(os.environ.get("APP_ID", 12606917)
-    API_HASH = os.environ.get("API_HASH", "f25113b8c17dca6fa7abda53a86bd4f7")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7115403757:AAHyzfrieb-faOXDW5lxSdQjM-NKHp9uULg")
+    APP_ID = int(os.environ.get("APP_ID", 23163380)
+    API_HASH = os.environ.get("API_HASH", "2dca155e786c7db2d295e5b4ab10783b")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
